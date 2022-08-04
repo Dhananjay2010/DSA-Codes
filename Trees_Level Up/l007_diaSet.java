aa4f4b74f2fce6bb33f42b4bf5069d1bf6b20921
Dhananjay2010 <dhananjay.rtr@gmail.com>
2022-08-04 10:01:03 +0530

    update

--- Trees_Level Up/l007_diaSet.java
+++ Trees_Level Up/l007_diaSet.java
@@ -94,17 +94,24 @@ public class l007_diaSet {
     // What we did was use a static variable to store the diameter and wrote the
     // function to calculate the height.
 
+    // Jab Hum static use karte hain, to faith ke words thode change ho jate hain.
+
+    // To yahan pe static diameter humare left diameter aur right diameter ke max to
+    // store karke chalta hai.
+
+    // ` Bas humne use root se pass hoti hui height se compare kiya hai
+
     int diameter = 0;
 
     public int diameterOfBinaryTree_03(TreeNode root) {
         if (root == null)
             return -1;
 
-        int ld = diameterOfBinaryTree_03(root.left);
-        int rd = diameterOfBinaryTree_03(root.right);
+        int lh = diameterOfBinaryTree_03(root.left);
+        int rh = diameterOfBinaryTree_03(root.right);
 
-        diameter = Math.max(diameter, ld + rd + 2);
-        return Math.max(ld, rd) + 1;
+        diameter = Math.max(diameter, lh + rh + 2);
+        return Math.max(lh, rh) + 1;
     }
 
     public int diameterOfBinaryTree_static(TreeNode root) {
@@ -174,8 +181,15 @@ public class l007_diaSet {
     }
 
     // b <===============Maximum path sum between two leaves =================>
-    // # Good Question(Pehli bar mai nhi hua)
+    // # Good Question(Pehli bar mai nhi hua, Dusri bar mai bhi nhi hua)
     // https://www.geeksforgeeks.org/find-maximum-path-sum-two-leaves-binary-tree/
+
+    // # Basic faith ye tha maine apne left ko bola ki mujhe to apna lefttoleft max
+    // # nikal ke dede. same maine right ko bola. Aur tab maine use apne se pass
+    // # hota hua leaf to leaf se compare kiya
+
+    // Apne se pass hone wale ke liye maine NodeToLeafMaxPathSum bhi return karwaya
+    // sath mai
 
     // ? Merepe basically teen chezein hain compare karne ke liye.
     // Aisa ho sakta hai ki mera max between two leaves left ke subtree ke 2 leaf ke
@@ -188,14 +202,97 @@ public class l007_diaSet {
     // karke mera left ke subtree ke k leaf se right ke subtree ke dusre leaf tak
     // ho sakta hai
 
-    // {LeafToLeafMaxPathSum, NodeToLeadMaxPathSum}
+    // {LeafToLeafMaxPathSum, NodeToLeafMaxPathSum}
     public static int[] maxPathSum(TreeNode root) {
+
+        if (root == null) {
+            return new int[] { -(int) 1e9, -(int) 1e9 };
+        }
+
+        if (root.left == null && root.right == null) {
+            return new int[] { -(int) 1e9, root.val }; // Agar mai ek leaf node hun to mai leafToleaf max path sum to
+                                                       // nhi nikal sakta, aur mera NodeToLeafMaxPathSum meri value ke
+                                                       // equall hoga
+        }
 
         int[] lp = maxPathSum(root.left);
         int[] rp = maxPathSum(root.right);
 
         int[] myAns = new int[2];
-        
+
+        // Agar mera left null hai ya right null hai to mai kabhi bhi apne se pass hota
+        // hua max path sum between two leaves to nikal he ni paunga. Aisa isiliye
+        // kyunki manle agar mera left null hai to mere left mai to koi leaf present to
+        // hoga he nhi. To isiliye ye wla Math.max() if se bahar likha hai
+
+        // For ex:- 9 in the gfg question diagram. Iske liye apply hoga ye.
+        // ! Important point
+        // For ex:- -1 kabhi apne se pass hota hua leaftoleaf bana he nhi payega. To
+
+        // isiliye if wali conction lagayi hai. Isiliya yahan se -∞,9 return hua.
+        myAns[0] = Math.max(lp[0], rp[0]);
+        if (root.left != null && root.right != null) {
+            myAns[0] = Math.max(myAns[0], lp[1] + rp[1] + root.val);
+        }
+
+        myAns[1] = Math.max(lp[1], rp[1]) + root.val; // Mere se kisi bhi leaf ka max kya hoga?? Jo bhi left aur right
+                                                      // se aaya, unka max nikala aut maine apni value mai add kar diya.
+
+        return myAns;
+
+    }
+
+    // ? Method 2 : Using the static variable
+
+    // # Yahan pe faith ke words change ho jate hain. yahan pe maine bola, jo maera
+    // # static variable hai, wo left ka LeafToLeafMaxPathSum aur right ka
+    // # LeafToLeafMaxPathSum ka max apne mai store kar leta hai. Bas mujhe yahan pe
+    // # use apne se pass hota hua leaf to leaf path se compare karna hai bas
+
+    // Humne yahan pe basically nodeToleaf return kara rahe hain bas
+
+    static int LeafToLeafMaxPathSum = -(int) 1e9;
+
+    public static int maxPathSum_02(TreeNode root) {
+        if (root == null) {
+            return -(int) 1e9;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+
+        int lnl = maxPathSum_02(root.left); // left node to leaf
+        int rnl = maxPathSum_02(root.right);// right node to leaf
+
+        if (root.left != null && root.right != null) {
+            LeafToLeafMaxPathSum = Math.max(LeafToLeafMaxPathSum, lnl + root.val + rnl);
+        }
+
+        return Math.max(lnl, rnl) + root.val;
+    }
+
+    public static int maxPathSum_gfg(TreeNode root) {
+        LeafToLeafMaxPathSum = -(int) 1e9;
+
+        // ! Below is a good way to tackle the situation
+        if (root.left == null || root.right == null) { // This done to pass in gfg. They have considered if the root has
+                                                       // only single child, then they are considering it a leaf node.
+                                                       // So therefore attached a node with 0 value.
+
+            // For test case like 5 N 6 -5 5, their ans was 16
+            if (root.left == null) {
+                root.left = new TreeNode(0);
+            } else {
+                root.right = new TreeNode(0);
+            }
+        }
+
+        int ans = maxPathSum_02(root);
+        // Also for a skew tree, there test case was giving an ans of root to leaf path,
+        // therefore added the below condition to pass.
+        return LeafToLeafMaxPathSum == -(int) 1e9 ? ans : LeafToLeafMaxPathSum;
+
     }
 
     // b <================Path Sum III (Not solved yet..) =====================>
