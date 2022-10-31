9cfc4095e5c077cd5633cf74dee955a3defd1f10
Dhananjay2010 <dhananjay.rtr@gmail.com>
2022-10-31 08:46:25 +0530

    dp start

---
+++ Dynamic_Programming/l001_twoPointerSet.java
@@ -0,0 +1,10 @@
+public class l001_twoPointerSet {
+
+    // Jahan pe bhi return fire hoga, us value ko kahin pe store kiya jayega.
+    // dp ki length kya honi chahiye == > mai apne recursive tree mai sabse min
+    // value aur max value dekhni hai kitni aa rahi hai.
+    
+    // To mujhe anpe array ka size aisa rakhna padega ki mai dono min se lekar max
+    // tak mai index ki tarah use kar sakun ke unki value store kar paun kahun pe.
+    // To mera total dp ka size hoga (max -min + 1);
+}
