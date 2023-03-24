6c056677404bb066085f53e782d4080813d334aa
Dhananjay2010 <dhananjay.rtr@gmail.com>
2023-03-24 14:42:40 +0530

    started searching and sorting

--- Searching_Sorting/QuickSort.java
+++ Searching_Sorting/QuickSort.java
@@ -1,9 +1,13 @@
-public class QuickSort{
+public class QuickSort {
 
-    public static int segregateData(int[] arr, int si, int ei, int pivot){
-        // This function will place the pivot in the correct position in the array and will return the correct postion of the pivot element as if the array was sorted.
+    public 
+
+    public static int segregateData(int[] arr, int si, int ei, int pivot) {
+        // This function will place the pivot in the correct position in the array and
+        // will return the correct postion of the pivot element as if the array was
+        // sorted.
 
         
-        
+
     }
 }
