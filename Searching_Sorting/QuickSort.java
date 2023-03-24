3d875a27b7df023f0319a57b28ab274e4f7be478
Dhananjay2010 <83901321+Dhananjay2010@users.noreply.github.com>
2023-03-24 14:45:05 +0530

    swap

--- Searching_Sorting/QuickSort.java
+++ Searching_Sorting/QuickSort.java
@@ -1,6 +1,10 @@
 public class QuickSort {
 
-    public 
+    public static void swap(int[] arr, int p1, int p2){
+        int k=arr[p1];
+        arr[p1]=arr[p2];
+        arr[p2]=k;
+    }
 
     public static int segregateData(int[] arr, int si, int ei, int pivot) {
         // This function will place the pivot in the correct position in the array and
